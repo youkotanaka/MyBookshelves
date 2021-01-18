4112afeb2d5b7e125e7a8cce7dddf1146a494fba
youkotanaka <nigorie.kazuha@gmail.com>
2021-01-18 16:53:44 +0900

    0118

--- public/js/app.js
+++ public/js/app.js
@@ -2027,7 +2027,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'Button',
   props: {
--- resources/js/components/ButtonComponent.vue
+++ resources/js/components/ButtonComponent.vue
@@ -2,7 +2,6 @@
     <button v-on:click="deleteBook(book.id)">{{ btnName }}
     </button>
 </template>
-
 <script>
 export default {
     name: 'Button',
