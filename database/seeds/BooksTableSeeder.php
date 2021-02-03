bd67772f96f4a14ea5dd604fdbe40411cd0fbef5
youkotanaka <nigorie.kazuha@gmail.com>
2021-02-03 11:23:15 +0900

    0203

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -2,7 +2,6 @@
 
 use App\Book;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class BooksTableSeeder extends Seeder
 {
@@ -13,70 +12,11 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = [
-            'title' => 'ハイキュー！！',
-            'vol' => '1'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => 'ハイキュー！！',
-            'vol' => '45'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => 'CUT',
-            'vol' => '2020年4月号'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => 'PHPフレームワークLaravel入門第２版',
-            'vol' => ''
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => 'AERA',
-            'vol' => '2019年2/11号'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => '週刊少年ジャンプ',
-            'vol' => '2012年12月号'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => '週刊少年ジャンプ',
-            'vol' => '2020年33・34合併号'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => '真夏の方程式',
-            'vol' => ''
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => '容疑者Xの献身',
-            'vol' => ''
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => 'シャーロック・ホームズ全集',
-            'vol' => '3'
-        ];
-        DB::table('books')->insert($param);
-
-        $param = [
-            'title' => '図書館戦争',
-            'vol' => ''
-        ];
-        DB::table('books')->insert($param);
+        for ($i = 1; $i <= 10; $i++){
+            Book::create([
+                'title' => 'title'.$i,
+                'vol' => 'vol'.$i
+            ]);
+        }
     }
 }
