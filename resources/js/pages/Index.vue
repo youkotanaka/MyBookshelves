5529fb069d60da617f2ce959730a5b974cdc266f
youkotanaka <nigorie.kazuha@gmail.com>
2021-02-18 15:09:34 +0900

    0218

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
         <h1>Index</h1>
-        <div class="row justify-content-center">
-
-                <RouterLink to="/books">
-                    <ButtonComponent class="btn btn-menu" btnName="BookList" />
-                </RouterLink>
-                <RouterLink to="/books/create">
-                    <ButtonComponent class="btn btn-menu" btnName="ADD" />
-                </RouterLink>
-
+        <div>
+            <RouterLink to="/books">
+                <ButtonComponent class="btn btn-menu" btnName="BookList" />
+            </RouterLink>
+            <RouterLink to="/books/create">
+                <ButtonComponent class="btn btn-menu" btnName="ADD" />
+            </RouterLink>
         </div>
     </div>
 </template>
